918875b5fe176cc8dab39d8779c2502d759b7e02
Marko Parfeniuk <90197701+markoparfeniuk@users.noreply.github.com>
2023-09-11 22:15:35 +0300

    fix namings

--- L1/CaesarShiftСipher/main.py
+++ L1/CaesarShiftСipher/main.py
@@ -1,10 +1,10 @@
 # Ukrainian alphabet
 alphabet = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
 
-def shift_cipher_encode(text, shift):
+def shift_cipher_encryption(text, shift):
     return ''.join([alphabet[(alphabet.index(c) + shift) % len(alphabet)] if c in alphabet else c for c in text])
 
-def shift_cipher_decode(text, shift):
+def shift_cipher_decrypted(text, shift):
     return ''.join([alphabet[(alphabet.index(c) - shift) % len(alphabet)] if c in alphabet else c for c in text])
 
 
@@ -19,10 +19,10 @@ with open('вулиця.txt', 'w', encoding='utf-8') as f:
 with open('вулиця.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-# encode text
+# encrypt text
 shift = 24
-encoded_text = shift_cipher_encode(text, shift)
+encrypted_text = shift_cipher_encode(text, shift)
 
-# write encoded tect into вулиця (endoded).txt
-with open('вулиця (encoded).txt', 'w', encoding='utf-8') as f:
+# write encrypted tect into вулиця (encrypted).txt
+with open('вулиця (encrypted).txt', 'w', encoding='utf-8') as f:
     f.write(encoded_text)
